00a82a08526ca62d16c529a2c194b53ce10e4d52
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-08-02 13:52:21 +0200

    made me code better

--- Grate/Modules/Misc/HanSolo1000FalconCoolHat.cs
+++ Grate/Modules/Misc/HanSolo1000FalconCoolHat.cs
@@ -8,112 +8,126 @@ using Grate.Tools;
 using UnityEngine;
 using NetworkPlayer = NetPlayer;
 
-namespace Grate.Modules.Misc;
-
-public class HanSolo1000FalconCoolHat : GrateModule
+namespace Grate.Modules.Misc
 {
-    public static readonly string DisplayName = "HanSolo1000Falcons magical hat";
-    private static GameObject HanSolo1000FalconHat;
-
-    // y u changing this mah bro
-    protected override void OnEnable()
+    public class HanSolo1000FalconCoolHat : GrateModule
     {
-        if (!MenuController.Instance.Built) return;
-        base.OnEnable();
-        HanSolo1000FalconHat = Instantiate(Plugin.AssetBundle.LoadAsset<GameObject>("goudabuda"));
-        
-        NetworkPropertyHandler.Instance.OnPlayerModStatusChanged += OnPlayerModStatusChanged;
-        VRRigCachePatches.OnRigCached += OnRigCached;
-        
-        HanSolo1000FalconHat.transform.SetParent(GestureTracker.Instance.rightHand.transform, true);
-        HanSolo1000FalconHat.transform.localPosition = new Vector3(-0.4782f, 0.1f, 0.4f);
-        HanSolo1000FalconHat.transform.localRotation = Quaternion.Euler(9, 0, 0);
-        HanSolo1000FalconHat.SetActive(false);
-        
-        try
-        {
-            GestureTracker.Instance.rightGrip.OnPressed += ToggleHanSolo1000FalconHatOn;
-            GestureTracker.Instance.rightGrip.OnReleased += ToggleHanSolo1000FalconHatOff;
-        }
-        catch (Exception e)
-        {
-            Logging.Exception(e);
-        }
-    }
+        public static readonly string DisplayName = "HanSolo1000Falcons magical hat";
 
-    private void OnPlayerModStatusChanged(NetworkPlayer player, string mod, bool enabled)
-    {
-        if (mod == DisplayName && player != NetworkSystem.Instance.LocalPlayer)
+        private const string HatAssetName = "goudabuda";
+        private const string HanSolo1000FalconsUserIdHeIsSoCool = "A48744B93D9A3596";
+
+        private static GameObject localHat;
+
+        protected override void OnEnable()
         {
+            if (!MenuController.Instance.Built)
+                return;
+
+            base.OnEnable();
+
+            localHat = Instantiate(Plugin.AssetBundle.LoadAsset<GameObject>(HatAssetName));
+            localHat.transform.SetParent(GestureTracker.Instance.rightHand.transform, true);
+            localHat.transform.localPosition = new Vector3(-0.4782f, 0.1f, 0.4f);
+            localHat.transform.localRotation = Quaternion.Euler(9f, 0f, 0f);
+            localHat.SetActive(false);
+            
+            try
+            {
+                GestureTracker.Instance.rightGrip.OnPressed += ToggleHatOn;
+                GestureTracker.Instance.rightGrip.OnReleased += ToggleHatOff;
+            }
+            catch (Exception e)
+            {
+                Logging.Exception(e);
+            }
+
+            NetworkPropertyHandler.Instance.OnPlayerModStatusChanged += OnPlayerModStatusChanged;
+            VRRigCachePatches.OnRigCached += OnRigCached;
+        }
+
+        private void ToggleHatOn(InputTracker tracker) => localHat?.SetActive(true);
+        private void ToggleHatOff(InputTracker tracker) => localHat?.SetActive(false);
+
+        private void OnPlayerModStatusChanged(NetworkPlayer player, string mod, bool enabled)
+        {
+            if (mod != DisplayName || player == NetworkSystem.Instance.LocalPlayer)
+                return;
+
             if (enabled)
                 player.Rig().gameObject.GetOrAddComponent<NetHanSolo1000FalconHat>();
             else
                 Destroy(player.Rig().gameObject.GetComponent<NetHanSolo1000FalconHat>());
         }
-    }
 
-
-    private void ToggleHanSolo1000FalconHatOn(InputTracker tracker) => HanSolo1000FalconHat?.SetActive(true);
-    private void ToggleHanSolo1000FalconHatOff(InputTracker tracker) => HanSolo1000FalconHat?.SetActive(false);
-
-    protected override void Cleanup()
-    {
-        HanSolo1000FalconHat?.Obliterate();
-        if (GestureTracker.Instance != null)
+        private void OnRigCached(NetPlayer player, VRRig rig)
         {
-            GestureTracker.Instance.rightGrip.OnPressed -= ToggleHanSolo1000FalconHatOn;
-            GestureTracker.Instance.rightGrip.OnReleased -= ToggleHanSolo1000FalconHatOff;
+            rig?.gameObject?.GetComponent<NetHanSolo1000FalconHat>()?.Obliterate();
         }
 
-        if (NetworkPropertyHandler.Instance != null)
-            NetworkPropertyHandler.Instance.OnPlayerModStatusChanged -= OnPlayerModStatusChanged;
-    }
-
-    private void OnRigCached(NetPlayer player, VRRig rig) => rig?.gameObject?.GetComponent<NetHanSolo1000FalconHat>()?.Obliterate();
-    public override string GetDisplayName() => DisplayName;
-    public override string Tutorial() => "- hansolo1000falcon can make a wind barrier with this (if you're not hansolo1000falcon, bad bad)";
-
-    private class NetHanSolo1000FalconHat : MonoBehaviour
-    {
-        private GameObject HanSolo1000FalconHatNet;
-        private NetworkedPlayer networkedPlayer;
-
-        private void OnEnable()
+        protected override void Cleanup()
         {
-            networkedPlayer = gameObject.GetComponent<NetworkedPlayer>();
-            var rightHand = networkedPlayer.rig.rightHandTransform;
+            localHat?.Obliterate();
+            localHat = null;
 
-            HanSolo1000FalconHatNet = Instantiate(HanSolo1000FalconHat);
+            if (GestureTracker.Instance != null)
+            {
+                GestureTracker.Instance.rightGrip.OnPressed -= ToggleHatOn;
+                GestureTracker.Instance.rightGrip.OnReleased -= ToggleHatOff;
+            }
 
-            HanSolo1000FalconHatNet.transform.SetParent(rightHand);
-            HanSolo1000FalconHatNet.transform.localPosition = new Vector3(0.04f, 0.05f, -0.02f);
-            HanSolo1000FalconHatNet.transform.localRotation = Quaternion.Euler(78.4409f, 0, 0);
-            HanSolo1000FalconHatNet.transform.localScale = new Vector3(1f, 1f, 1f);
+            if (NetworkPropertyHandler.Instance != null)
+                NetworkPropertyHandler.Instance.OnPlayerModStatusChanged -= OnPlayerModStatusChanged;
 
-            networkedPlayer.OnGripPressed += OnGripPressed;
-            networkedPlayer.OnGripReleased += OnGripReleased;
-
-            if (networkedPlayer.owner.UserId != "A48744B93D9A3596")
-                HanSolo1000FalconHatNet.Obliterate();
+            VRRigCachePatches.OnRigCached -= OnRigCached;
         }
 
-        private void OnDestroy()
-        {
-            networkedPlayer.OnGripPressed -= OnGripPressed;
-            networkedPlayer.OnGripReleased -= OnGripReleased;
-            HanSolo1000FalconHatNet.Obliterate();
-        }
+        public override string GetDisplayName() => DisplayName;
 
-        private void OnGripPressed(NetworkedPlayer player, bool isLeft)
-        {
-            if (!isLeft)
-                HanSolo1000FalconHatNet.SetActive(true);
-        }
+        public override string Tutorial() =>
+            "- HanSolo1000Falcon can make a wind barrier with this hat. If you're not HanSolo1000Falcon... no hat for you.";
 
-        private void OnGripReleased(NetworkedPlayer player, bool isLeft)
+        private class NetHanSolo1000FalconHat : MonoBehaviour
         {
-            if (!isLeft)
-                HanSolo1000FalconHatNet.SetActive(false);
+            private GameObject hatInstance;
+            private NetworkedPlayer networkedPlayer;
+
+            private void OnEnable()
+            {
+                networkedPlayer = GetComponent<NetworkedPlayer>();
+                var rightHand = networkedPlayer.rig?.rightHandTransform;
+
+                hatInstance = Instantiate(localHat);
+                hatInstance.transform.SetParent(rightHand);
+                hatInstance.transform.localPosition = new Vector3(0.04f, 0.05f, -0.02f);
+                hatInstance.transform.localRotation = Quaternion.Euler(78.4409f, 0f, 0f);
+                hatInstance.transform.localScale = Vector3.one;
+
+                networkedPlayer.OnGripPressed += HandleGripPressed;
+                networkedPlayer.OnGripReleased += HandleGripReleased;
+
+                if (networkedPlayer.owner.UserId != HanSolo1000FalconsUserIdHeIsSoCool)
+                    hatInstance.Obliterate();
+            }
+
+            private void OnDestroy()
+            {
+                networkedPlayer.OnGripPressed -= HandleGripPressed;
+                networkedPlayer.OnGripReleased -= HandleGripReleased;
+                hatInstance?.Obliterate();
+            }
+
+            private void HandleGripPressed(NetworkedPlayer player, bool isLeft)
+            {
+                if (!isLeft)
+                    hatInstance?.SetActive(true);
+            }
+
+            private void HandleGripReleased(NetworkedPlayer player, bool isLeft)
+            {
+                if (!isLeft)
+                    hatInstance?.SetActive(false);
+            }
         }
     }
 }
