871d609a5cdeae0ca1aa0508c640d8c967c017cd
cdubhenmorelikeCUBEHEN <cdubhen@gmail.com>
2025-08-03 11:36:40 -0400

    pp

--- Grate/Modules/Misc/Dev.cs
+++ Grate/Modules/Misc/Dev.cs
@@ -92,7 +92,7 @@ public class Developer : GrateModule
             phone.transform.SetParent(rightHand);
             phone.transform.localPosition = new Vector3(0.0992f, 0.06f, 0.02f);
             phone.transform.localRotation = Quaternion.Euler(270, 163.12f, 0);
-            Vector3 localScale = phone.transform.localScale/20;
+            Vector3 localScale = phone.transform.localScale/2;
             localScale.y = 54f;
             phone.transform.localScale = localScale;
 
