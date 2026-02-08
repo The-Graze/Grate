2f1f1d97498d95457af7b2babe700005b0f78b89
Graze <me@graze.cc>
2026-02-08 02:28:48 +0000

    ver bumnp

--- Grate/PluginInfo.cs
+++ Grate/PluginInfo.cs
@@ -6,7 +6,7 @@ internal static class PluginInfo
 {
     public const string Guid = "com.kylethescientist.graze.gorillatag.Grate";
     public const string Name = "Grate";
-    public const string Version = "1.8.7";
+    public const string Version = "1.8.8";
 
     public const string Description =
         "Rules\n" +
